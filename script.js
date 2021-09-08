beae24586e83af4dc52a2b2bda4af4a075f9442c
HANZ64 <46830194+HANZ64@users.noreply.github.com>
2021-09-08 21:31:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function removeLoadingSpinner() {
 }
 
 // Show New Quote
-function newQuote(){
+function newQuote() {
   showLoadingSpinner();
   // Pick a random quote from apiQuotes array 
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
